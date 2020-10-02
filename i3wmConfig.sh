497a16416556c48a65c71c1237e0b6eb9fbb1630
DeadS3c <52109756+DeadS3c@users.noreply.github.com>
2020-10-02 11:36:07 +0200

    Update i3wmConfig.sh

--- i3wmConfig.sh
+++ i3wmConfig.sh
@@ -70,49 +70,6 @@ sudo updatedb # Update mlocate db
 # Config pacman
 sudo cp mirrorupgrade.hook /etc/pacman.d/hooks/
 sudo cp pacman.conf /etc/
-# Config ArchStrike
-# https://archstrike.org/wiki/setup
-sudo pacman -Syy --noconfirm --needed
-
-sudo pacman-key --init
-sudo dirmngr < /dev/null
-wget https://archstrike.org/keyfile.asc
-sudo pacman-key --add keyfile.asc
-pacman-key --lsign-key 9D5F1C051D146843CDA4858BDE64825E7CBC0D51
-
-sudo pacman -S --noconfirm archstrike-keyring
-sudo pacman -S --noconfirm archstrike-mirrorlist
-
-sudo sed -i 's/.*mirror.archstrike.*/Include = \/etc\/pacman.d\/archstrike-mirrorlist/' /etc/pacman.conf
-sudo pacman -Syy --noconfirm --needed
-
-sleep 1
-
-echo "Installing tools with archstrike"
-sudo pacman -S --noconfirm --needed wfuzz dirbuster
-sudo pacman -S --noconfirm --needed burpsuite
-sudo pacman -S --noconfirm --needed crunch cupp-git cewl
-sudo pacman -S --noconfirm --needed netdiscover
-sudo pacman -S --noconfirm --needed dirsearch
-sudo pacman -S --noconfirm --needed hash-identifier
-sudo pacman -S --noconfirm --needed dnsenum sublist3r-git dnsrecon
-sudo pacman -S --noconfirm --needed amass
-sudo pacman -S --noconfirm --needed enum4linux
-sudo pacman -S --noconfirm --needed crackmapexec
-sudo pacman -S --noconfirm --needed wafw00f
-sudo pacman -S --noconfirm --needed whatweb-git
-sudo pacman -S --noconfirm --needed recon-ng-git
-sudo pacman -S --noconfirm --needed shellter
-sudo pacman -S --noconfirm --needed theharvester-git
-sudo pacman -S --noconfirm --needed metagoofil
-sudo pacman -S --noconfirm --needed smtp-user-enum
-sudo pacman -S --noconfirm --needed fierce-git
-# For windows
-sudo pacman -S --noconfirm --needed windows-binaries
-sudo pacman -S --noconfirm --needed mimikatz
-sudo pacman -S --noconfirm --needed onesixtyone
-# For wifi
-sudo pacman -S --noconfirm --needed wifiphisher-git wifijammer-git
 
 sleep 1
 
